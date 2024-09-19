e481ac25557dbf80720bf1d73428cc95bfadbea6
Tzvi <you@example.com>
2024-09-19 17:56:04 +0300

    create from json

--- go.bat
+++ go.bat
@@ -1,6 +1,6 @@
 git init
 git add .
-git commit -m "first commit"
+git commit -m "create from json"
 git branch -M main
 git remote add origin https://github.com/pachshish/nbaTest.git
 git push -u origin main
